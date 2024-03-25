145c2044d9a5704a6eb95a09274e1347196ebe09
Aviad Bagno <aviad.bagno@gmail.com>
2024-03-25 19:51:10 +0200

    Calc len instead of inversion

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 int current_level;
 unsigned long long found = 0;
 unsigned long long *polynom;
-unsigned int max_inversion = 0;
+unsigned int max_len = 0;
 
 // Returns a new, dynamically allocated, permutation. Exits program on error.
 int *allocate_perm()
@@ -255,74 +255,9 @@ void cleanup()
 	free_found_tree();
 }
 
-// Sets r to be p(s)
-void perms_composition(const int *p, const int *s, int *r)
+unsigned int number_of_cycles(const int *perm)
 {
-	for (int i = 0; i < K; i++)
-	{
-		r[i] = p[s[i] - 1];
-	}
-}
-
-// Finds the minimum inversion of p*(c^i)*s
-unsigned int find_min_inversion(int *p, int *s)
-{
-	unsigned int min_inv = UINT_MAX;
-	for (int i = 0; i < K; i++)
-	{
-		int comp[K];
-		perms_composition(p, s, comp);
-		unsigned int inv = get_inversion(comp);
-		if (inv < min_inv)
-			min_inv = inv;
-
-		shift_left(s); // Next shift. (Finally will cancel out).
-	}
-
-	return min_inv;
-}
-
-void calculate_values_iterator(int *perm, int level)
-{
-	static int phase = 1;
-	static int *phase1_perm;
-
-	if (phase == 1)
-	{
-		// We only have one perm, we need to go over all perms with this perm.
-		phase = 2;
-		phase1_perm = perm;
-		iterate_permutations(&calculate_values_iterator);
-		phase = 1;
-	}
-	else
-	{
-		// We have two perms!
-		unsigned int min_inv = find_min_inversion(phase1_perm, perm);
-		if (min_inv > max_inversion)
-			max_inversion = min_inv;
-
-		if (PRINT_PERMS)
-		{
-			printf("Minimum inversion for ");
-			print_perm(phase1_perm);
-			printf(" and ");
-			print_perm(perm);
-			printf(" is: %d\n", min_inv);
-		}
-	}
-}
-
-void calculate_values()
-{
-	// Iterate over all pairs of permutations.
-	iterate_permutations(&calculate_values_iterator);
-	printf("Max inversion: %d\n", max_inversion);
-}
-
-int number_of_cycles(const int *perm)
-{
-	int cycles = 0;
+	unsigned int cycles = 0;
 	bool visited[K];
 	memset(visited, 0, K * sizeof(bool));
 
@@ -341,6 +276,71 @@ int number_of_cycles(const int *perm)
 	}
 
 	return cycles;
+}
+
+// Sets r to be p(s)
+void perms_composition(const int *p, const int *s, int *r)
+{
+	for (int i = 0; i < K; i++)
+	{
+		r[i] = p[s[i] - 1];
+	}
+}
+
+// Finds the minimum len of p*(c^i)*s
+unsigned int find_min_len(int *p, int *s)
+{
+	unsigned int min_l = UINT_MAX;
+	for (int i = 0; i < K; i++)
+	{
+		int comp[K];
+		perms_composition(p, s, comp);
+		unsigned int l = N - number_of_cycles(comp);
+		if (l < min_l)
+			min_l = l;
+
+		shift_left(s); // Next shift. (Finally will cancel out).
+	}
+
+	return min_l;
+}
+
+void calculate_values_iterator(int *perm, int level)
+{
+	static int phase = 1;
+	static int *phase1_perm;
+
+	if (phase == 1)
+	{
+		// We only have one perm, we need to go over all perms with this perm.
+		phase = 2;
+		phase1_perm = perm;
+		iterate_permutations(&calculate_values_iterator);
+		phase = 1;
+	}
+	else
+	{
+		// We have two perms!
+		unsigned int min_l = find_min_len(phase1_perm, perm);
+		if (min_l > max_len)
+			max_len = min_l;
+
+		if (PRINT_PERMS)
+		{
+			printf("Minimum len for ");
+			print_perm(phase1_perm);
+			printf(" and ");
+			print_perm(perm);
+			printf(" is: %d\n", min_l);
+		}
+	}
+}
+
+void calculate_values()
+{
+	// Iterate over all pairs of permutations.
+	iterate_permutations(&calculate_values_iterator);
+	printf("Max len: %d\n", max_len);
 }
 
 int main()
